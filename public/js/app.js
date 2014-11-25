e6c4a776dae1732e7dca15c009c68b39b76b90d0
jaon <joao.domingos@stepmoney.com.br>
2014-11-25 14:02:28 -0300

    Sumiu menu

--- public/js/app.js
+++ public/js/app.js
@@ -98,14 +98,7 @@ angular.module('App', ['ionic'])
   var data = {
     labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho"],
     datasets: [
-      {
-        label: "My First dataset",
-        fillColor: "rgba(220,220,220,0.5)",
-        strokeColor: "rgba(220,220,220,0.8)",
-        highlightFill: "rgba(220,220,220,0.75)",
-        highlightStroke: "rgba(220,220,220,1)",
-        data: [65, 59, 80, 81, 56, 55, 40]
-      },
+
       {
         label: "My Second dataset",
         fillColor: "rgba(151,187,205,0.5)",
